e85aed28b68b41dfefdd4dc26ef45d281f55278f
SeongBeom lee <52270698+SeongBeom27@users.noreply.github.com>
2022-02-18 19:58:03 +0900

    Broadcast 사용

--- server/src/main.js
+++ server/src/main.js
@@ -12,10 +12,19 @@ server.listen(process.env.PORT || port, () => {
   console.log(`app run at : http://127.0.0.1:${port}`);
 });
 
+// conection 이벤트 처리 함수 추가
 io.on('connection', (socket) => {
-  console.log('socket initialization completed');
-  socket.on('say', (data) => {
-    console.log(data, 'received information');
-    socket.emit('news', { hello: 'hey!!! you~!~!~!~' });
+  console.log('connect');
+
+  socket.on('message', (data) => {
+    console.log('data 이벤트를 받았습니다.');
+    // console.dir(data);
+
+    if (data.recepient == 'ALL') {
+      // 나를 포함한 모든 클라이언트에게 메시지 전달
+      console.dir('나를 포함한 모든 클라이언트에게 data 이벤트를 전송합니다.');
+      // 서버에 연결된 모든 클라이언트의 소켓들이 sockets에 들어있음
+      io.sockets.emit('message', data);
+    }
   });
 });
